847d8ae456379eda34a29a626f909a3e6b651333
Agung Narayana Adhi Putra <narayanaagung@student.telkomuniversity.ac.id>
2021-09-09 12:38:17 +0700

    3.4.Menambahkan Gambar Hewan

--- app/src/main/java/org/d3if4202/remidial/MainActivity.kt
+++ app/src/main/java/org/d3if4202/remidial/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     private val hewan = listOf("Ayam","Bebek","Domba","Kambing","Sapi")
@@ -18,6 +19,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun showNext(){
         index = if(index==hewan.size-1)0 else index+1
+        val imageView: ImageView = findViewById(R.id.imageView)
+        val resourceId = when(index) {
+            1 -> R.drawable.bebek
+            2 -> R.drawable.domba
+            3 -> R.drawable.kambing
+            4 -> R.drawable.sapi
+            else -> R.drawable.ayam
+        }
+        imageView.setImageResource(resourceId)
         val textView:TextView=findViewById(R.id.nameTextView)
         textView.text=hewan[index]
     }
