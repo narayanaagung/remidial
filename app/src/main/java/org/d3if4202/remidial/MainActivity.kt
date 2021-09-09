1f55bcd41fa587c7cd66dd439894f448a59ba5b5
Agung Narayana Adhi Putra <narayanaagung@student.telkomuniversity.ac.id>
2021-09-09 12:27:54 +0700

    3.1.Menambahkan Tombol Next

--- app/src/main/java/org/d3if4202/remidial/MainActivity.kt
+++ app/src/main/java/org/d3if4202/remidial/MainActivity.kt
@@ -2,10 +2,19 @@ package org.d3if4202.remidial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nextButton:Button = findViewById(R.id.nextButton)
+        nextButton.setOnClickListener{showNext()}
+    }
+
+    private fun showNext(){
+        Log.d("MainActivity","Clicked")
     }
 }
