6be5e84d342c7e56438734aad0163f8ced488dd7
Agung Narayana Adhi Putra <narayanaagung@student.telkomuniversity.ac.id>
2021-09-09 12:45:21 +0700

    challenges

--- app/src/main/java/org/d3if4202/remidial/MainActivity.kt
+++ app/src/main/java/org/d3if4202/remidial/MainActivity.kt
@@ -31,4 +31,18 @@ class MainActivity : AppCompatActivity() {
         val textView:TextView=findViewById(R.id.nameTextView)
         textView.text=hewan[index]
     }
+    private fun showPrevious(){
+        if (index == 0) index = 4 else index--
+        val imageView: ImageView = findViewById(R.id.imageView)
+        val resourceId = when(index) {
+            1 -> R.drawable.bebek
+            2 -> R.drawable.domba
+            3 -> R.drawable.kambing
+            4 -> R.drawable.sapi
+            else -> R.drawable.ayam
+        }
+        imageView.setImageResource(resourceId)
+        val textView: TextView = findViewById(R.id.nameTextView)
+        textView.text = hewan[index]
+    }
 }
