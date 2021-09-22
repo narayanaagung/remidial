6edbc9b2b37f1f7d87f5871454b183384a8667f7
Agung Narayana Adhi Putra <narayanaagung@student.telkomuniversity.ac.id>
2021-09-22 09:16:47 +0700

    3.3.Menambahkan view binding

--- app/src/main/java/org/d3if4202/remidial/MainActivity.kt
+++ app/src/main/java/org/d3if4202/remidial/MainActivity.kt
@@ -3,11 +3,14 @@ package org.d3if4202.remidial
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import org.d3if4202.remidial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         Log.d("MainActivity", "Jumlah data: " + getData().size)
     }
     private fun getData(): List<Hewan> {
