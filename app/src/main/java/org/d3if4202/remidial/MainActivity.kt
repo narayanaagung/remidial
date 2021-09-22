c39e68f886f8ba47a009b420e67d0156835fd6a5
Agung Narayana Adhi Putra <narayanaagung@student.telkomuniversity.ac.id>
2021-09-22 08:42:33 +0700

    3.2.Menambahkan view binding

--- app/src/main/java/org/d3if4202/remidial/MainActivity.kt
+++ app/src/main/java/org/d3if4202/remidial/MainActivity.kt
@@ -2,10 +2,21 @@ package org.d3if4202.remidial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import org.d3if4202.remidial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{hitungBmi()}
+    }
+
+    private fun hitungBmi(){
+        Log.d("Main Activity","Tombol di click")
     }
 }
